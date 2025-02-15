4b9c2ef7703ee928a3eb89bc971f5dbf1eb3e4f5
Salino3 <flaviobcn012@gmail.com>
2025-02-15 11:23:47 +0100

    add detector languages

--- src/i18next/i18n.ts
+++ src/i18next/i18n.ts
@@ -1,26 +1,42 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector"; // Import the language detector
 import { en as enHome } from "./home/en";
 import { es as esHome } from "./home/es";
 import { en as enDashboard } from "./dashboard/en";
 import { es as esDashboard } from "./dashboard/es";
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: {
-      home: enHome,
-      dashboard: enDashboard,
+// Use the language detector
+i18n
+  .use(LanguageDetector) // Add the language detector
+  .use(initReactI18next) // Use React i18next
+  .init({
+    resources: {
+      en: {
+        home: enHome,
+        dashboard: enDashboard,
+      },
+      es: {
+        home: esHome,
+        dashboard: esDashboard,
+      },
     },
-    es: {
-      home: esHome,
-      dashboard: esDashboard,
+    lng: localStorage.getItem("lng") || undefined, // use detector to decide the language (use localStorage value if available)
+    fallbackLng: "en", // Default language if the user's language is not supported
+    detection: {
+      order: [
+        "navigator",
+        "localStorage",
+        "cookie",
+        "htmlTag",
+        "path",
+        "subdomain",
+      ], // Order of language detection
+      caches: ["localStorage", "cookie"], // Cache the detected language in localStorage or cookies
     },
-  },
-  lng: localStorage.getItem("lng") || "en", // Default language
-  fallbackLng: "en", // Default value for default fallback
-  interpolation: {
-    escapeValue: false,
-  },
-});
+    interpolation: {
+      escapeValue: false, // No need for escaping, React handles this
+    },
+  });
 
 export default i18n;
