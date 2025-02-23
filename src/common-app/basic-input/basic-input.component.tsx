cef9a7b0971194bc242cb29cbe1c0e1e9aabe55f
Salino3 <flaviobcn012@gmail.com>
2025-02-23 18:10:38 +0100

    more parameters

--- src/common-app/basic-input/basic-input.component.tsx
+++ src/common-app/basic-input/basic-input.component.tsx
@@ -10,26 +10,37 @@ interface PropsBasicInput {
   change?: React.ChangeEventHandler<HTMLInputElement> | undefined;
   ref?: React.LegacyRef<HTMLInputElement> | undefined;
   errMsg?: string;
+  checkError?: boolean;
 }
 
 export const BasicInput: React.FC<PropsBasicInput> = (props) => {
-  const { type, name, customStyles, lbl, click, change, ref, errMsg } = props;
+  const {
+    type,
+    name,
+    customStyles,
+    lbl,
+    click,
+    change,
+    ref,
+    errMsg,
+    checkError = false,
+  } = props;
   return (
     <div ref={ref} className={`containerBasicInput ${customStyles}`}>
       <div className="contentInputBI">
         <label htmlFor={name}>{lbl}</label>
         <input
+          className={`${checkError ? "inputError" : ""}`}
           // It works after focus
           //   pattern="[A-Za-z]{3,10}"
           //   required
           //   onInvalid={handleInvalid} // Handle errors
-
           id={name}
           name={name}
           type={type}
           onClick={click}
           onChange={change}
-          //   onInput={() => alert("Hi!")} // It works when value change
+          // onInput={() => alert("Hi!")} // It works when value change
         />
       </div>
       {errMsg && <small>{errMsg}</small>}
