eae8b495b88cc78de7c5609873680a5fc423ab3d
Salino3 <flaviobcn012@gmail.com>
2025-02-28 11:17:26 +0100

    add some styles to svg X

--- src/common-app/input-photos/input-photos.component.tsx
+++ src/common-app/input-photos/input-photos.component.tsx
@@ -17,6 +17,64 @@ interface Props {
   click?: React.MouseEventHandler<HTMLDivElement> | undefined;
   ref?: React.LegacyRef<HTMLInputElement> | undefined;
 }
+
+// Working in progress
+export const ContainerMultiFiless: React.FC<{
+  file: (string & any) | (File & any);
+}> = ({ file }) => {
+  return (
+    <div className="containerMultiFiless">
+      <div className="card">
+        <span>{file instanceof File && file?.name}</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <line x1="18" y1="6" x2="6" y2="18" />
+          <line x1="6" y1="6" x2="18" y2="18" />
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+//*
+export const ContainerFile: React.FC<{
+  fileName: AccountRegisterForm["profile_picture"] | string;
+}> = ({ fileName }) => {
+  return (
+    <div className="containerFile">
+      <span>{fileName instanceof File && fileName?.name}</span>
+      <svg
+        onClick={(event) => {
+          event?.preventDefault();
+          event?.stopPropagation();
+        }}
+        className="svg_01"
+        xmlns="http://www.w3.org/2000/svg"
+        width="26"
+        height="24"
+        viewBox="0 0 24 24"
+        // fill="green"
+        // stroke="var(--color-17)"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <line x1="18" y1="6" x2="6" y2="18" />
+        <line x1="6" y1="6" x2="18" y2="18" />
+      </svg>
+    </div>
+  );
+};
+
 export const InputPhotos: React.FC<Props> = (props) => {
   const {
     multiple,
@@ -36,11 +94,7 @@ export const InputPhotos: React.FC<Props> = (props) => {
   const { t } = useTranslation("common");
   console.log("here1", fileName);
   return (
-    <div
-      onClick={click}
-      ref={ref}
-      className={`rootInputPhotos ${customStyles}`}
-    >
+    <div onClick={click} ref={ref} className={`rootInputFiles ${customStyles}`}>
       <span>{lbl}</span>
       <label htmlFor={name} className="customFileLabel">
         <div className="boxInputFiles">
@@ -57,11 +111,13 @@ export const InputPhotos: React.FC<Props> = (props) => {
             className={`${checkError ? "inputError" : ""}`}
           />
         </div>
-        {Array.isArray(fileName) && multiple && fileName?.length > 0
-          ? fileName.map((file) => <div>{file?.name}</div>)
-          : fileName instanceof File && fileName?.name
-          ? fileName?.name
-          : t("no_file_selected")}
+        {Array.isArray(fileName) && multiple && fileName?.length > 0 ? (
+          fileName.map((file) => <ContainerMultiFiless file={file} />)
+        ) : fileName instanceof File && fileName?.name ? (
+          <ContainerFile fileName={fileName} />
+        ) : (
+          t("no_file_selected")
+        )}
       </label>
       {errMsg && <small>{errMsg}</small>}
     </div>
