ac00192d97c938c2bf8f022d41ae09c9e7a93efd
Salino3 <flaviobcn012@gmail.com>
2025-03-02 11:03:39 +0100

    commit

--- src/common-app/lateral-bar/lateral-bar.component.tsx
+++ src/common-app/lateral-bar/lateral-bar.component.tsx
@@ -8,6 +8,7 @@ export const LateralBar: React.FC = memo(() => {
 
   const {
     showPersonalInfo,
+    setShowPersonalInfo,
     state: { currentAccount, theme },
   } = useContext(GlobalAppContext);
 
@@ -18,65 +19,102 @@ export const LateralBar: React.FC = memo(() => {
   console.log("hello", currentAccount);
   return (
     <div id={theme} className="rootLateralBar">
-      <div className="boxArrow">
-        <svg
-          width="67"
-          height="89"
-          viewBox="0 0 67 89"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <line
-            x1="0.953561"
-            y1="1.8421"
-            x2="51.9536"
-            y2="43.8421"
-            stroke="currentColor"
-            stroke-width="3"
-          />
-          <line
-            x1="51.9398"
-            y1="46.1691"
-            x2="0.939838"
-            y2="87.1691"
-            stroke="currentColor"
-            stroke-width="3"
-          />
-          <line
-            x1="51.9398"
-            y1="46.1691"
-            x2="0.939838"
-            y2="87.1691"
-            stroke="currentColor"
-            stroke-width="3"
-          />
-          <line
-            x1="14.9536"
-            y1="1.8421"
-            x2="65.9536"
-            y2="43.8421"
-            stroke="currentColor"
-            stroke-width="3"
-          />
-          <line
-            x1="65.9398"
-            y1="46.1691"
-            x2="14.9398"
-            y2="87.1691"
-            stroke="currentColor"
-            stroke-width="3"
-          />
-          <line
-            x1="65.9398"
-            y1="46.1691"
-            x2="14.9398"
-            y2="87.1691"
-            stroke="currentColor"
-            stroke-width="3"
-          />
-        </svg>
+      <div
+        className="boxArrow"
+        onClick={() => setShowPersonalInfo((prev) => !prev)}
+      >
+        {showPersonalInfo ? (
+          <svg
+            width="67"
+            height="89"
+            viewBox="0 0 89 89"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <line
+              x1="0.953561"
+              y1="1.8421"
+              x2="51.9536"
+              y2="43.8421"
+              stroke="currentColor"
+              strokeWidth="3"
+            />
+            <line
+              x1="51.9398"
+              y1="46.1691"
+              x2="0.939838"
+              y2="87.1691"
+              stroke="currentColor"
+              strokeWidth="3"
+            />
+            <line
+              x1="30.9536"
+              y1="1.8421"
+              x2="80.9536"
+              y2="43.8421"
+              stroke="currentColor"
+              strokeWidth="3"
+            />
+
+            <line
+              x1="80.9398"
+              y1="46.1691"
+              x2="30.9398"
+              y2="87.1691"
+              stroke="currentColor"
+              strokeWidth="3"
+            />
+          </svg>
+        ) : (
+          <svg
+            className="rotateIcon"
+            width="67"
+            height="89"
+            viewBox="0 0 89 89"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <line
+              x1="0.953561"
+              y1="1.8421"
+              x2="51.9536"
+              y2="43.8421"
+              stroke="currentColor"
+              strokeWidth="3"
+            />
+            <line
+              x1="51.9398"
+              y1="46.1691"
+              x2="0.939838"
+              y2="87.1691"
+              stroke="currentColor"
+              strokeWidth="3"
+            />
+            <line
+              x1="30.9536"
+              y1="1.8421"
+              x2="80.9536"
+              y2="43.8421"
+              stroke="currentColor"
+              strokeWidth="3"
+            />
+
+            <line
+              x1="80.9398"
+              y1="46.1691"
+              x2="30.9398"
+              y2="87.1691"
+              stroke="currentColor"
+              strokeWidth="3"
+            />
+          </svg>
+        )}
       </div>
-      <h3>{currentAccount?.username}</h3>
+      {showPersonalInfo && (
+        <div className="containerLateralBar">
+          <h3>{currentAccount?.username}</h3>
+        </div>
+      )}
     </div>
   );
 });
