7d1cd7a9d11056378caf41df6ae58c9a1ba9313f
Salino3 <flaviobcn012@gmail.com>
2025-03-08 11:27:06 +0100

    download image company downLoadImage

--- src/pods/dashboard/components/card-companies/card-companies.component.tsx
+++ src/pods/dashboard/components/card-companies/card-companies.component.tsx
@@ -1,36 +1,13 @@
 import { TFunction } from "i18next";
 import { Company } from "../../../../core/companies";
+import { useAppFunctions } from "../../../../hooks";
 import "./card-companies.styles.scss";
 
 export const CardCompanies: React.FC<{
   company: Company;
   t: TFunction<"dashboard", undefined>;
 }> = ({ company, t }) => {
-  function downLoadImage(logo: string) {
-    if (!logo) return;
-
-    fetch(logo)
-      .then((response) => response.blob()) // Convert the image to a Blob
-      .then((blob) => {
-        const url = window.URL.createObjectURL(blob);
-
-        // Obtener la extensión del archivo de la URL
-        const extension = logo.split(".").pop()?.split("?")[0] || "png"; // Si no tiene extensión, usa 'png'
-        const fileName = `company_logo.${extension}`;
-
-        // Crear un enlace de descarga
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = fileName;
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-
-        // Liberar el objeto URL
-        window.URL.revokeObjectURL(url);
-      })
-      .catch((error) => console.error("Error downloading the image:", error));
-  }
+  const { downLoadImage } = useAppFunctions();
 
   return (
     <div key={company?.company_id} className="cardComapny">
